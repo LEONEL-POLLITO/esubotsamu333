792b9daa2a7852d8b4f0941641eb486b2ebf3236
LEONEL-POLLITO <81774877+LEONEL-POLLITO@users.noreply.github.com>
2021-04-11 16:52:38 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '529984907794', '👑Samu330👑', m)
+  // this.sendContact(m.chat, '51996897568', 'Nurutomo', m)
+  this.sendContact(m.chat, '51996897568', '👑MERRY👑', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
